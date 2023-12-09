024f2352e887c9c35cae4a16da8753cc1916b371
Vadzim Kurdzesau <vkurdzesau@gmail.com>
2023-12-09 23:24:47 +0100

    feat: partially implement user screen

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 ﻿from kivy.lang import Builder
 
 from kivymd.app import MDApp
-from kivymd.uix.scrollview import MDScrollView
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.menu import MDDropdownMenu
+
+from model.user import User
 
 KV = '''
 MDScreen:
@@ -27,7 +30,7 @@ MDScreen:
                     text: "Добро пожаловать!"
                     halign: "center"
 
-            MDScreen:
+            UserInfoScreen:
                 name: "src_user_info"
 
                 MDLabel:
@@ -50,7 +53,76 @@ MDScreen:
                         root.nav_drawer.set_state("close")
                         root.screen_manager.current = "src_user_info"
 
+<UserInfoScreen>:
+    MDBoxLayout:
+        orientation: 'vertical'
+        padding: dp(20), dp(60), dp(20), dp(20)  # Left, Top, Right, Bottom
+        spacing: dp(10)
+
+        # MDTextField:
+        #     id: user_id_field
+        #     hint_text: 'User ID'
+        #     helper_text: 'Enter your user ID'
+        #     helper_text_mode: 'on_focus'
+
+        MDTextField:
+            id: weight_field
+            hint_text: 'Вес (в кг)'
+            helper_text: 'Enter your weight in kg'
+            helper_text_mode: 'on_focus'
+            input_filter: 'float'
+            text: root.user.weight.__str__()
+
+        MDTextField:
+            id: height_field
+            hint_text: 'Рост (в см)'
+            helper_text: 'Enter your height in cm'
+            helper_text_mode: 'on_focus'
+            input_filter: 'float',
+            text: root.user.height.__str__()
+
+        MDTextField:
+            id: age_field
+            hint_text: 'Возраст'
+            helper_text: 'Enter your age'
+            helper_text_mode: 'on_focus'
+            input_filter: 'int'
+            text: root.user.age.__str__()
+
+        MDBoxLayout:
+            orientation: 'horizontal'
+            MDLabel:
+                text: 'Пол: '
+                size_hint_x: None
+                width: dp(48)
+
+            MDDropDownItem:
+                id: sex_select
+                text: 'Gender'
+                on_release: root.genders.open()
+
+            MDSwitch:
+                id: sex_switch
+                # 'active' will be True if the switch is on (male) and False if it's off (female)
+
+        MDRaisedButton:
+            text: 'Обновить'
+            on_release: root.save_user_data()
+
+
 '''
+
+class UserInfoScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = User("V", 12, 12, 12, True, 12)
+        self.gendersDict = {
+            "Male": True,
+            "Female": False
+        }
+    
+    def save_user_data(self):
+        pass
 
 class Example(MDApp):
     def build(self):
--- path.py
+++ path.py
@@ -1,1 +1,1 @@
-LOCAL_PATH = "C:\Users\vkurd\source\repos\Learn\Polina\physiological_indicators_analisis_app"
+LOCAL_PATH = "C:\\Users\\vkurd\\source\\repos\\Learn\\Polina\\physiological_indicators_analisis_app"
