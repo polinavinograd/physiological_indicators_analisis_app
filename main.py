40256fb264ff9dc34395dfd3fbf0cf2ac2ea806d
Vadzim Kurdzesau <vkurdzesau@gmail.com>
2023-12-09 19:42:10 +0100

    feat: define main screen

--- main.py
+++ main.py
@@ -1,10 +1,62 @@
-﻿from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+﻿from kivy.lang import Builder
 
+from kivymd.app import MDApp
+from kivymd.uix.scrollview import MDScrollView
 
-class MainApp(MDApp):
+KV = '''
+MDScreen:
+    screen_manager: screen_manager
+    nav_drawer: nav_drawer
+
+    # Объявлен вне навигации, чтобы присутствовал на каждом экране
+    MDTopAppBar:
+        pos_hint: {"top": 1}
+        elevation: 4
+        title: "Мое приложение"
+        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+
+    MDNavigationLayout:
+
+        MDScreenManager:
+            id: screen_manager
+
+            MDScreen:
+                name: "scr_main_screen"
+
+                MDLabel:
+                    text: "Добро пожаловать!"
+                    halign: "center"
+
+            MDScreen:
+                name: "src_user_info"
+
+                MDLabel:
+                    text: "User info"
+                    halign: "center"
+
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+            close_on_click: "false"
+            
+            MDNavigationDrawerMenu:
+                screen_manager: screen_manager
+                nav_drawer: nav_drawer
+
+                MDNavigationDrawerItem:
+                    icon: "account-box-outline"
+                    text: "Мои данные"
+                    on_press:
+                        root.nav_drawer.set_state("close")
+                        root.screen_manager.current = "src_user_info"
+
+'''
+
+class Example(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(KV)
 
 
-MainApp().run()
+Example().run()
