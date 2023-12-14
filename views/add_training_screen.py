eefb2f46b5ba1c365c92d1a889d75b9f1a6de17d
Vadzim Kurdzesau <vkurdzesau@gmail.com>
2023-12-14 02:32:51 +0100

    feat: complete training screen

--- views/add_training_screen.py
+++ views/add_training_screen.py
@@ -1,10 +1,10 @@
 ﻿from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.boxlayout import MDBoxLayout
-from views.shared_components import InputTextField, ListItem, SaveableInputInteger, DatePickerButton, SelectableList, SaveableInputString
+from views.shared_components import DropDownList, DropDownListItem, InputTextField, ListItem, SaveableInputInteger, DatePickerButton, SelectableList, SaveableInputString
 from kivymd.app import MDApp
 from kivy.metrics import dp
-from typing import List
+from typing import List, Tuple
 from model.calories_module import CaloriesModule
 
 class NewTrainingRecord:
@@ -27,6 +27,24 @@ class NewTrainingRecord:
     @minutes.setter
     def minutes(self, minutes: int) -> None:
         self.__minutes.save_value(minutes)
+        
+class SelectedTrainingRecord:
+    def __init__(self, exercise_name: str, calorie: int, minutes: int) -> None:
+        self.__exercise_name = exercise_name
+        self.__calorie = calorie
+        self.__minutes = minutes
+
+    @property
+    def exercise_name(self) -> str:
+        return self.__exercise_name
+    
+    @property
+    def calorie(self) -> int:
+        return self.__calorie
+        
+    @property
+    def minutes(self) -> int:
+        return self.__minutes
 
 class AddTrainingBox(MDBoxLayout):
     def __init__(self, *args, **kwargs):
@@ -36,11 +54,16 @@ class AddTrainingBox(MDBoxLayout):
         self.padding=(dp(20), dp(0), dp(20), dp(20))
         self.spacing=dp(20)
 
-        self.__new_training_record = NewTrainingRecord()
+        data_storage = MDApp.get_running_app().data_storage
+        available_exercises = data_storage.get_exercises_list()
 
-        self.__ingredient_input = InputTextField(self.__new_training_record.training_name, title='Введите упражнение')
+        # Ключ - название, значение - калории
+        items = self.__get_exercises_list(available_exercises)
+        self.__available_exercises_drop_down_list = DropDownList(items, size_hint=(0.6, 1))
+
+        self.__time_entered = SaveableInputInteger()
         self.__mass_input = InputTextField(
-            self.__new_training_record.minutes,
+            self.__time_entered,
             title='Время (в минутах)',
             input_filter='int',
             size_hint=(0.8, 1))
@@ -51,34 +74,46 @@ class AddTrainingBox(MDBoxLayout):
             pos_hint={'center_x': 0.5, 'center_y': 0.5})
         save_button.bind(on_release=self.add_training)
 
-        self.__trainings = SelectableList(size_hint=(0.6, 1))
-
         self.add_widget(MDBoxLayout(
-            self.__ingredient_input,
+            self.__available_exercises_drop_down_list,
             self.__mass_input,
             save_button,
             orientation='horizontal',
             size_hint=(1, 0.2)
         ))
 
-        self.add_widget(self.__trainings)
+        self.__selected_trainings = SelectableList(size_hint=(1, 1))
+        self.add_widget(self.__selected_trainings)
         
-    def get_selected_trainings(self) -> List[NewTrainingRecord]:
-        selected_items = self.__trainings.get_selected_items()
+    def __get_exercises_list(self, exercises: List[Tuple[str,int]]) -> List[DropDownListItem]:
+        result = []
+        for exercise in exercises:
+            result.append(DropDownListItem(exercise[0], exercise[1]))
+
+        return result
+        
+    def get_selected_exercises(self) -> List[SelectedTrainingRecord]:
+        selected_items = self.__selected_trainings.get_selected_items()
         return list(map(lambda item: item.value, selected_items))
 
     def add_training(self, *args):
-        if self.__new_training_record.training_name.get_value() == '':
+        time_entered = self.__time_entered.get_value()
+        if time_entered <= 0:
             return
         
-        list_item = ListItem(
-            id=self.__new_training_record.training_name.get_value(),
-            value=self.__new_training_record,
-            is_selected=True
-        )
+        # Ключ - название, значение - калории
+        item_selected = self.__available_exercises_drop_down_list.selected_item
 
-        self.__trainings.add_item(
-            text=self.__new_training_record.training_name.get_value(),
+        list_item = ListItem(
+            id=item_selected.title,
+            value=SelectedTrainingRecord(
+                exercise_name=item_selected.title,
+                calorie=item_selected.value,
+                minutes=time_entered),
+            is_selected=True)
+
+        self.__selected_trainings.add_item(
+            text=list_item.id,
             item=list_item)
 
 class AddTrainingScreen(MDScreen):
@@ -99,17 +134,15 @@ class AddTrainingScreen(MDScreen):
             ))
 
     def add_training_entry(self, *args):
-        added_ingredients = self.__trainings_box.get_selected_trainings()
-        if added_ingredients.__len__() == 0:
+        added_exercises = self.__trainings_box.get_selected_exercises()
+        if added_exercises.__len__() == 0:
             return
         
-        # TODO: сохранять записи о тренировках
+        data_storage = MDApp.get_running_app().data_storage
+        user_data = MDApp.get_running_app().user
+        calories_module = CaloriesModule(user_data, data_storage)
+        
+        selected_date = self.__date_picker.selected_date
 
-        # data_storage = MDApp.get_running_app().data_storage
-        # user_data = MDApp.get_running_app().user
-        # calories_module = CaloriesModule(user_data, data_storage)
-
-        # selected_date = self.__date_picker.selected_date
-
-        # ingredients_and_mass = { record.training_name.get_value(): record.minutes.get_value() for record in added_ingredients }
-        # calories_module.set_calories_intake(selected_date.__str__(), ingredients_and_mass)
+        exercises_and_minutes = { record.exercise_name: record.minutes for record in added_exercises }
+        calories_module.set_calories_outcome(selected_date.__str__(), exercises_and_minutes)
